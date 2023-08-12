2910fc7adc539fc7de0328bc343d3aaad01ec3dc
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-08-12 22:50:42 +0530

    update

--- src/pages/Departments.jsx
+++ src/pages/Departments.jsx
@@ -1,20 +1,32 @@
 import React, { useContext } from "react";
 import { ProductsContext } from "../context/ProductsContext";
+import { FilterContext } from "../context/FilterContext";
+import { useNavigate } from "react-router-dom";
 
 export const Departments = () => {
   const { allProducts } = useContext(ProductsContext);
+  const { filterDispatch } = useContext(FilterContext);
+
+  const navigate = useNavigate();
 
   const departments = allProducts.map((p) => p.department);
   const uniqueDepartment = new Set(departments);
+
+  const HandleDepartmentFilter = (e) => {
+    filterDispatch({ type: "SET_CATEGORY", payload: e.target.id });
+    navigate("/products");
+  };
   return (
     <>
       <div className="flex gap-10 justify-between border ">
         {Array.from(uniqueDepartment).map((d) => (
           <div
             key={d}
+            id={d}
+            onClick={HandleDepartmentFilter}
             className="bg-gray-300 p-2 text-center shadow-lg cursor-pointer "
           >
-            <p>{d}</p>
+            {d}
           </div>
         ))}
       </div>
