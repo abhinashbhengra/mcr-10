c223bb2ae50e31778e059428858340aee4d504d7
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-08-12 23:13:26 +0530

    update

--- src/components/DepartmentFilter.jsx
+++ src/components/DepartmentFilter.jsx
@@ -1,11 +1,18 @@
 import { useContext } from "react";
 import { ProductsContext } from "../context/ProductsContext";
+import { FilterContext } from "../context/FilterContext";
 
 export const DepartmentFilter = ({ departments, filterDispatch }) => {
   const { allProducts } = useContext(ProductsContext);
+  const { filterState } = useContext(FilterContext);
 
   const departmentsfromDB = allProducts.map((p) => p.department);
   const uniqueDepartment = Array.from(new Set(departmentsfromDB));
+
+  const checkSelected = (value) => {
+    console.log("clicked", filterState.categories);
+    return filterState.categories === value;
+  };
 
   const handleDepartment = (e) => {
     filterDispatch({ type: "SET_CATEGORY", payload: e.target.value });
@@ -15,9 +22,11 @@ export const DepartmentFilter = ({ departments, filterDispatch }) => {
     <>
       <div>
         <select name="department" onChange={handleDepartment}>
-          <option value="All">All Department</option>
+          <option value="All" selected={"All" === filterState.categories}>
+            All Department
+          </option>
           {uniqueDepartment.map((d) => (
-            <option key={d} value={d}>
+            <option key={d} value={d} selected={d === filterState.categories}>
               {d}
             </option>
           ))}
